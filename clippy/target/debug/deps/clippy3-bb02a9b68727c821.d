02cdaac531fe12b80226bafcf9c10fc07ede3ba8
Malina55555 <malinovka55555@yandex.ru>
2023-07-16 20:09:33 +0300

    DZ_6

---
+++ clippy/target/debug/deps/clippy3-bb02a9b68727c821.d
@@ -0,0 +1,8 @@
+/home/annauser/Rust/rustlings/exercises/clippy/target/debug/deps/clippy3-bb02a9b68727c821.rmeta: clippy3.rs Cargo.toml
+
+/home/annauser/Rust/rustlings/exercises/clippy/target/debug/deps/clippy3-bb02a9b68727c821.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
